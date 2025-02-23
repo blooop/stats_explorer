182913b42bb90f80850654bf41dd9ccaca4a5afa
Austin Gregg-Smith <blooop@gmail.com>
2025-02-23 16:47:33 +0000

    1d sweeps

--- stats_explorer/stats.py
+++ stats_explorer/stats.py
@@ -5,6 +5,8 @@ import random
 class FalsePositives(bch.ParametrizedSweep):
     """This class has 0 input dimensions and 1 output dimensions.  It samples from a gaussian distribution"""
 
+    prevalence = bch.FloatSweep(default=1, bounds=[1, 10], units="%")
+    false_positive_rate = bch.FloatSweep(default=0.02, bounds=[0.01, 0.1], units="%")
     # This defines a variable that we want to plot
     diseased = bch.ResultVar(units="ul")
     healthy = bch.ResultVar(units="ul")
@@ -12,16 +14,16 @@ class FalsePositives(bch.ParametrizedSweep):
     random_var = bch.ResultVar()
 
     def __call__(self, **kwargs) -> dict:
-        prevalence = 0.01
-        false_positive_rate = 0.02
+
+        # self.prevalence = 1
 
         self.random_var = random.random()
-        is_diseased = self.random_var < prevalence
-        self.diseased = 1 if is_diseased else 0
-        self.healthy = 1 if not is_diseased else 0
+        is_diseased = self.random_var < self.prevalence / 100.0
+        self.diseased = 1. if is_diseased else 0.
+        self.healthy = 1. if not is_diseased else 0.
 
         if not is_diseased:
-            self.test_result = 1 * (random.random() < false_positive_rate)
+            self.test_result = 1 * (random.random() < self.false_positive_rate / 100.0)
         else:
             self.test_result = 0.0
 
@@ -34,7 +36,8 @@ def example_0_in_1_out(
     """This example shows how to sample a 1 dimensional float variable and plot the result of passing that parameter sweep to the benchmarking function"""
 
     bench = FalsePositives().to_bench(run_cfg, report)
-    bench.plot_sweep()
+    res=bench.plot_sweep(input_vars=["prevalence"])
+    bench.report.append(res.to_scatter())
     return bench
 
 
