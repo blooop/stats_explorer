47bee920069ae61561aa6cc8bbf3f8d5b2813d1b
Austin Gregg-Smith <blooop@gmail.com>
2025-02-23 14:53:45 +0000

    update pixi.lock

--- stats_explorer/stats.py
+++ stats_explorer/stats.py
@@ -6,24 +6,24 @@ class FalsePositives(bch.ParametrizedSweep):
     """This class has 0 input dimensions and 1 output dimensions.  It samples from a gaussian distribution"""
 
     # This defines a variable that we want to plot
-    diseased = bch.ResultVar(units="ul" )
+    diseased = bch.ResultVar(units="ul")
     healthy = bch.ResultVar(units="ul")
     test_result = bch.ResultVar(units="ul")
-    rv = bch.ResultVar()
+    random_var = bch.ResultVar()
 
     def __call__(self, **kwargs) -> dict:
         prevalence = 0.01
         false_positive_rate = 0.02
 
-        self.rv = random.random()
-        is_diseased = self.rv < prevalence
+        self.random_var = random.random()
+        is_diseased = self.random_var < prevalence
         self.diseased = 1 if is_diseased else 0
         self.healthy = 1 if not is_diseased else 0
 
         if not is_diseased:
             self.test_result = 1 * (random.random() < false_positive_rate)
         else:
-            self.test_result = 0.
+            self.test_result = 0.0
 
         return super().__call__(**kwargs)
 
